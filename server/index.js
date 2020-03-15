46a96d1dfa07cf9dd5d8054444a16a8e8603eb8a
per18020 <per18020@byui.edu>
2020-03-14 22:29:23 -0600

    debugging

--- server/index.js
+++ server/index.js
@@ -1,41 +1,24 @@
 const path = require('path');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
 
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
 
 const SocketAPI = require('./socketAPI');
 
-// Multi-process to utilize all CPU cores.
-if (!isDev && cluster.isMaster) {
-  console.error(`Node cluster master ${process.pid} is running`);
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
 
-  // Fork workers.
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-  });
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function (request, response) {
+  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+});
 
-} else {
-  const express = require('express');
-  const app = express();
-  const server = require('http').Server(app);
+new SocketAPI(server);
 
-  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get('*', function(request, response) {
-    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-  });
-
-  new SocketAPI(server);
-  
-  // Start 'er up!
-  server.listen(PORT, function () {
-    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-  });
-}
+// Start 'er up!
+server.listen(PORT, function () {
+  console.error(`Node ${isDev ? 'dev server' : 'cluster worker ' + process.pid}: listening on port ${PORT}`);
+});
