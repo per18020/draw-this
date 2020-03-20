ff3f124811d9dc72096728470fe6deeb22e79381
per18020 <per18020@byui.edu>
2020-03-19 21:36:12 -0600

    debugging

--- react-ui/src/common/util.js
+++ react-ui/src/common/util.js
@@ -1,12 +1,12 @@
 import LZString from 'lz-string';
 
 export function stringifyAndCompress(obj) {
-    return LZString.compress(JSON.stringify(obj));
+    return LZString.compressToUTF16(JSON.stringify(obj));
 }
 
 export function decompressAndParse(str) {
     console.log(str);
-    console.log(LZString.decompress(str));
+    console.log(LZString.decompressFromUTF16(str));
     console.log(JSON.parse(LZString.decompress(str)));
-    return JSON.parse(LZString.decompress(str));
+    return JSON.parse(LZString.decompressFromUTF16(str));
 }
